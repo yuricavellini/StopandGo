2fd5492189d6f96ba4ce04732dd38ca9424cc259
unknown <ST10517330@myemeris.edu.za>
2026-02-25 14:20:12 +0200

    Fianl Submission

---
+++ src/program1ant/Program1Ant.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package program1ant;
+
+/**
+ *
+ * @author User
+ */
+public class Program1Ant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("              ---------\n\n" //This code will print stop and go signs
+                         + "   ********   \t\t   XXXXXXXX\n"// /t used to create spaces
+                         + " *          * \t\t X          X\n"// /n used to skip to the line below 
+                         + "*    STOP    *\t\tX     GO     X\n"
+                         + " *          * \t\t X          X\n"
+                         + "   ********   \t\t   XXXXXXXX\n"
+                         + "      **      \t\t      XX\n"
+                         + "      **      \t\t      XX\n"
+                         + "      **      \t\t      XX\n"
+                         + "      **      \t\t      XX\n"
+                         + "     ****     \t\t     XXXX\n"
+                         + "  **********  \t\t  XXXXXXXXXX\n"
+                         + "|------------------------------------|\n"
+                         + "|------------------------------------|");
+    }                    
+    
+}
